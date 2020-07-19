4f69b536f93d85c847d7e9e82a02aa262f996336
Melanie Rush <merush@microsoft.com>
2020-07-19 14:31:20 -0600

    first commit

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const NotFoundPage = () => {
+  return(
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+import { PageProps } from "gatsby"
+
+export default function IndexRoute(props: PageProps) {
+  return (
+    <>
+      <h1>Path:</h1>
+      <p>{props.path}</p>
+    </>
+  )
+}
